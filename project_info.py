b11b2ef2c83773f22cffa7502c29d93d3ada007e
Wim Christiaansen <wimchristiaansen@gmail.com>
2020-07-31 13:52:28 +0200

    Add files via upload

---
+++ project_info.py
@@ -0,0 +1,15 @@
+"""
+
+Step 1
+Create the Pipeline for your code
+
+Step 2
+Put your file into a Database (TIP: use a PostgreSQL database on your computer, because large file will cost a lot to store online)
+
+Step 3 Pipeline Creation
+Make a Flask API for it
+
+Step 4 API Creation
+Put it on a Web Service
+
+"""
